50e9f14c90685a6b257bf8aecbe3ab18b1ded120
Emre Cakar <emre_147123@hotmail.com>
2023-12-13 21:09:39 +0100

    işlem önceliği

---
+++ Islemonceligi/src/IslemOnceligi.java
@@ -0,0 +1,13 @@
+public class IslemOnceligi {
+    public static void main(String[] args) {
+        int a,b,c,result;
+        a = 10;
+        b = 2;
+        c = 3;
+        result = a+b*c-b;
+System.out.print(result);
+
+
+
+   }
+}
