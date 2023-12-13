c95e8bf5dd8206a4eafa79a0a71c486332dce1a6
Emre Cakar <emre_147123@hotmail.com>
2023-12-13 21:16:52 +0100

    islem_onceligi

--- Islemonceligi/src/IslemOnceligi.java
+++ Islemonceligi/src/IslemOnceligi.java
@@ -1,13 +1,20 @@
+import java.util.Scanner;
 public class IslemOnceligi {
     public static void main(String[] args) {
-        int a,b,c,result;
-        a = 10;
-        b = 2;
-        c = 3;
-        result = a+b*c-b;
-System.out.print(result);
+        Scanner input = new Scanner(System.in);
+        double a,b,c,result;
 
+        System.out.print("a değerini giriniz :");
+        a = input.nextDouble();
 
+        System.out.print("b değerini giriniz :");
+        b = input.nextDouble();
+
+        System.out.print("c değerini giriniz :");
+        c = input.nextDouble();
+
+        result= a+b*c-b;
+        System.out.print(result);
 
    }
 }
