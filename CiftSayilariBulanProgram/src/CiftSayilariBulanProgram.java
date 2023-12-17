c8f51e167d183363dea3aa144c9f0950994f576f
Emre Cakar <emre_147123@hotmail.com>
2023-12-17 21:49:55 +0100

    Cift_Sayilari_Bulan_Program

--- CiftSayilariBulanProgram/src/CiftSayilariBulanProgram.java
+++ CiftSayilariBulanProgram/src/CiftSayilariBulanProgram.java
@@ -5,11 +5,16 @@ public class CiftSayilariBulanProgram {
         Scanner input = new Scanner(System.in);
         int number, average, kadar, sum = 0;
 
+        // Kullanıcıdan bir sayı girmesini istiyorum.
+
         System.out.print("Bir sayı giriniz: ");
         number = input.nextInt();
-        int i = 12;
 
-        int count = 0; // Count of numbers
+        // Kullanıcıdan aldığım sayıyı 3 ün ve 4 ün ortak en küçük katı olan 12 ye göre baz alarak yapıyorum.
+        // i'yi 12 ile başlatıyorum ortalama da 0'ı da dahil etmesini istemedim.
+        // count burada kaç sayının toplandığını bize belirtiyor.
+        int i = 12;
+        int count = 0;
 
         do {
             sum += i;
